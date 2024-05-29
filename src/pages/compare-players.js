5789c4ab06fe00e5c2ca0eb53fed3b4ba1f8a8d6
YoyoSoliman <151338070+YoyoSoliman@users.noreply.github.com>
2024-05-28 17:37:37 -0700

    Add files via upload

---
+++ src/pages/compare-players.js
@@ -0,0 +1,21 @@
+//Yehia Soliman
+//5.1.2024
+
+import React from "react";
+
+const Compare = () => {// This if for the compare page
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "centre",
+                alignItems: "centre",
+                height: "100vh",
+            }}
+        >
+            <h1>Compare different players</h1>
+        </div>
+    );
+};
+
+export default Compare;
---
+++ src/pages/player-stats.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PlayerStats({ playerName }) {
+    return (
+        <div>
+            <h2>Player Stats for {playerName}</h2>
+            {/* Render player stats data here */}
+        </div>
+    );
+}
+
+export default PlayerStats;
